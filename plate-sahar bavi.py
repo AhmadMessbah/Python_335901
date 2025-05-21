491ea4e736bbdec50265b18b76fecb1bb5a88dd8
saharbavi <saharbavi04@gmail.com>
2025-05-22 01:49:25 +0330

    تمرین جلسه هفتم پلاک ماشین

--- plate-sahar bavi.py
+++ plate-sahar bavi.py
@@ -3,13 +3,15 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+
 def open_new_form():
     window.destroy()
     new_form = Tk()
     new_form.mainloop()
 
+
 window = Tk()
-window.title("Person Info")
+window.title("Plate Number")
 window.geometry("1250x300")
 
 # Id
@@ -22,17 +24,17 @@ my_label = Label(window, image=img)
 my_label.place(x=20, y=20)
 
 # Button(window, image=img,command=open_new_form).place(x=20, y=200)
-part1 = StringVar(value = "12")
-Entry(window, textvariable=part1,width=2, font=("B Traffic", 120)).place(x=140,y=50, height=220 , width=200)
+part1 = IntVar(value=12)
+Entry(window, textvariable=part1, font=("B Traffic", 120)).place(x=140, y=50, height=220, width=200)
 
-part2 = StringVar(value = "ب")
-Entry(window, textvariable=part2,width=2, font=("B Traffic", 120)).place(x=350,y=50,  height=220 , width=200)
+part2 = StringVar(value="ب")
+Entry(window, textvariable=part2, font=("B Traffic", 120)).place(x=350, y=50, height=220, width=200)
 
-part3 = StringVar(value = "123")
-Entry(window, textvariable=part3,width=2, font=("B Traffic", 120)).place(x=530,y=50,  height=220 , width=400)
+part3 = IntVar(value=123)
+Entry(window, textvariable=part3, font=("B Traffic", 120)).place(x=530, y=50, height=220, width=400)
 
-part4 = StringVar(value = "12")
-Entry(window, textvariable=part4,width=2, font=("B Traffic", 120)).place(x=960,y=98,  height=180 , width=198)
+part4 = IntVar(value=12)
+Entry(window, textvariable=part4, font=("B Traffic", 120)).place(x=960, y=98, height=180, width=198)
 
 part1.get()
 part2.get()
